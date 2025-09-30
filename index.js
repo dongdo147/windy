277dac526bc22dd64a52ac1675d9974539be3485
dongdo147 <tekuhasirama@gmail.com>
2025-09-30 09:57:52 +0700

    'dong123'

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-require('dotenv').config();
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 
-// Nếu chỉ chạy serverless function trên Vercel, index.js có thể rất đơn giản
-// Hoặc có thể để trống, tùy project. 
+// Route trả HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-module.exports = app;
+// Route trả JS
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+});
+
+// Route API config trả key
+app.get('/config', (req, res) => {
+    res.json({ windyKey: process.env.KEY });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
